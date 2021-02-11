2a656fb1a4635a7598a94591ed98d022a5eb8af9
bgani <ganibdev@gmail.com>
2021-02-11 11:51:26 +0600

    Initial project

---
+++ dotnet-parallel/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_parallel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
